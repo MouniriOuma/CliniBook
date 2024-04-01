02036c6f28ffa5b73a069dee68219f2da772a6c7
Lenovo <mouniri.ouma@gmail.com>
2024-04-01 16:15:20 +0000

    add routes

---
+++ backend/routes/center.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const router = express.Router()
+
+// GET all center
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all centers'})
+  })
+  
+  // GET a single center
+  router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single center'})
+  })
+  
+  // POST a new center
+  router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new center'})
+  })
+  
+  // DELETE a center
+  router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a center'})
+  })
+  
+  // UPDATE a center
+  router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a center'})
+  })
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
+const centerRoutes = require('./routes/center')
 
 //express app
 const app = express()
 
-//routes
-app.get('/', (req, res) =>{
-    res.json({mssg:'welcome'})
+//midleware
+app.
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
 })
 
+//routes
+app.use('/api/center', centerRoutes)
+
 //requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log('listening on port 4000')
 })
