ba79d945a6dc12b74cce19ea111303765b5d491c
Lenovo <mouniri.ouma@gmail.com>
2024-04-08 02:41:01 +0000

    add uri

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const centerRoutes = require('./routes/center')
+const appointmentRoutes = require('./routes/appointment')
+const timeslotRoutes = require('./routes/timeslot')
+const userRoutes = require('./routes/user')
+
 
 //express app
 const app = express()
@@ -17,6 +21,10 @@ app.use((req, res, next) =>{
 
 //routes
 app.use('/api/center', centerRoutes)
+app.use('/api/appointment', appointmentRoutes)
+app.use('/api/timeSlot', timeslotRoutes)
+app.use('/api/user', userRoutes)
+
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
