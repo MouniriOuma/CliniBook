622d5c3b3b3ab9a094e214a3282f16bc0577dc1b
Lenovo <mouniri.ouma@gmail.com>
2024-04-08 02:39:41 +0000

    updates

--- backend/routes/timeslot.js
+++ backend/routes/timeslot.js
@@ -11,18 +11,18 @@ const {
 const router = express.Router()
 
 // GET all Timeslots
-router.get('/', getTimeslots)
+router.get('/', getTimeSlots)
 
 // GET a single Timeslot
-router.get('/:id', getTimeslot)
+router.get('/:id', getTimeSlot)
 
 // POST a new Timeslot
-router.post('/', createTimeslot)
+router.post('/', createTimeSlot)
 
 // DELETE a Timeslot
-router.delete('/:id', deleteTimeslot)
+router.delete('/:id', deleteTimeSlot)
 
 // UPDATE a Timeslot
-router.patch('/:id', updateTimeslot)
+router.patch('/:id', updateTimeSlot)
 
 module.exports = router
