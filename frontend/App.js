f9cf1113e57b65db76b61ab87b795dd923927a7a
Lenovo <mouniri.ouma@gmail.com>
2024-04-21 04:59:43 +0000

    add sign up page

--- frontend/App.js
+++ frontend/App.js
@@ -2,23 +2,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, StatusBar, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { AuthContextProvider } from './src/context/AuthContext';
 
 //pages and components
 import Login from './src/pages/login'
+import SignUp from './src/pages/SignUp';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1}}>
-      
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
-              <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-              {/* <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />                   */}
-        </Stack.Navigator>
-      </NavigationContainer>
-
+      <AuthContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Login'>
+            <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+            <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthContextProvider>
     </SafeAreaView>
   );
 }
